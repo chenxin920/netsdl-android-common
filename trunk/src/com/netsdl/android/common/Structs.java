3c66bcfac1c97e7e7f41f8dfe6bf8ea6c8dd0d42
jasper.sun@gmail.com <jasper.sun@gmail.com@aab1c51b-4dae-30dc-8959-415835ad6c83>
2012-11-26 08:22:29 +0000

    增加Item的折扣

--- trunk/src/com/netsdl/android/common/Structs.java
+++ trunk/src/com/netsdl/android/common/Structs.java
@@ -3,10 +3,10 @@ package com.netsdl.android.common;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-public class Structs implements Serializable{
+public class Structs implements Serializable {
 	private static final long serialVersionUID = 7140676115707227296L;
 
-	public class Item implements Serializable{
+	public class Item implements Serializable {
 		private static final long serialVersionUID = 6751519151605241578L;
 
 		public Item() {
@@ -21,12 +21,12 @@ public class Structs implements Serializable{
 			Item itemNew = new Item();
 
 			itemNew.price = price.add(new BigDecimal("0"));
-			if(count!=null)
+			if (count != null)
 				itemNew.count = new Integer(count);
 			else
 				itemNew.count = new Integer(0);
 			itemNew.lumpSum = lumpSum.add(new BigDecimal("0"));
-			if(itemDiscount!=null)
+			if (itemDiscount != null)
 				itemNew.itemDiscount = new Integer(itemDiscount);
 			else
 				itemNew.itemDiscount = new Integer(100);
@@ -34,7 +34,7 @@ public class Structs implements Serializable{
 		}
 	}
 
-	public enum Type implements Serializable{
+	public enum Type implements Serializable {
 		type1 {
 			public DocumentType toDocumentType() {
 				return DocumentType.DO;
@@ -73,7 +73,7 @@ public class Structs implements Serializable{
 
 	}
 
-	public enum DocumentType implements Serializable{
+	public enum DocumentType implements Serializable {
 		DO {
 			public String toString() {
 				return "DO";
@@ -87,7 +87,7 @@ public class Structs implements Serializable{
 
 	}
 
-	public enum RtnType implements Serializable{
+	public enum RtnType implements Serializable {
 		P1 {
 			public String toString() {
 				return "1";
@@ -102,6 +102,7 @@ public class Structs implements Serializable{
 
 	public class DeviceItem implements Serializable {
 		private static final long serialVersionUID = 3344079987392960944L;
+
 		public DeviceItem() {
 
 		}
@@ -114,27 +115,24 @@ public class Structs implements Serializable{
 		public String documentDate;
 		public String[] operator;
 		public String remarks;
-		
+
 		public int intStart;
 		public String printWSDL;
 		public String printNameSpace;
 		public String printMethod;
 		public String printFlagIn;
 		public String printFlagOut;
-		
-		
 
 	}
-	
+
 	public class LoginViewData implements Serializable {
 		private static final long serialVersionUID = 5956154244068859349L;
 		public LoginStatus status = LoginStatus.operaterID;
 		public Object[] storeObjs;
 		public String text = "";
-		
 
 	}
-	
+
 	public enum LoginStatus implements Serializable {
 		operaterID, password
 	}
